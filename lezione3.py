5aca4706d21e2e215d076ac1da96193abb739856
madlyuk <lucabellomi@gmail.com>
2023-07-23 16:35:45 +0200

    lez3 aggiunta bocciatura

--- lezione3.py
+++ lezione3.py
@@ -51,38 +51,53 @@ print("------------ ESEMPIO 2 --------------")
 
 
 print("------------ ESEMPIO 3 --------------")
-alunni = [] # --> ora ne inseriamo più di uno con un ciclo WHILE!
-alunno = {}
-esci = False
+# alunni = [] # --> ora ne inseriamo più di uno con un ciclo WHILE!
+# esci = False
 
-print("Ora ti chiedo di inserire qualche dato dell'alunno: ")
-# GUARDARE LA CONVERSIONE DI TIPO E COME MODELLO LE STRINGHE!
+# print("Ora ti chiedo di inserire qualche dato dell'alunno: ")
+# # GUARDARE LA CONVERSIONE DI TIPO E COME MODELLO LE STRINGHE!
 
-while not esci:
-    # SET dei valori nel dizionario
-    alunno["nome"] = input("inserisci il nome: ").capitalize()
-    alunno["cognome"] = input("inserisci il cognome: ").capitalize()
-    alunno["età"] = int(input("inserisci l'età: "))
-    alunno["classe"] = int(input("inserisci la classe: "))
-    alunno["sezione"] = input("inserisci la sezione: ").upper()
-    end = input("Vuoi inserire un altro alunno? Scrivi 's' per SI o 'n' per NO: ").upper()
+# while not esci:
+#     # SET dei valori nel dizionario
+#     alunno = {}
+#     alunno["nome"] = input("inserisci il nome: ").capitalize()
+#     alunno["cognome"] = input("inserisci il cognome: ").capitalize()
+#     alunno["età"] = int(input("inserisci l'età: "))
+#     alunno["classe"] = int(input("inserisci la classe: "))
+#     alunno["sezione"] = input("inserisci la sezione: ").upper()
+#     end = input("Vuoi inserire un altro alunno? Scrivi 's' per SI o 'n' per NO: ").upper()
     
-    # Aggiungo l'alunno alla mia lista di alunni
-    alunni.append(alunno)
+#     # Aggiungo l'alunno alla mia lista di alunni
+#     alunni.append(alunno)
     
-    if end == "N": # mi semplifico la vita mettendo in maiuscolo per non fare due controlli
-        esci = True
+#     if end == "N": # mi semplifico la vita mettendo in maiuscolo per non fare due controlli
+#         esci = True
 
 
-for studende in alunni:
-    # GET dei valori del dizionario
-    scheda_alunno = f"""
-        -----Alunno: {alunno.get("nome")} {alunno.get("cognome")}----
-        età: {alunno.get("età")}
-        classe/sezione: {alunno.get("classe")}{alunno.get("sezione")}
-        -------------------------------------------------------------
-    """
-    print(scheda_alunno)
+# for alunno in alunni:
+    
+#     # aggiungo l'informazione se c'è una bocciatura!
+#     classe = alunno.get("classe")
+#     anni = alunno.get("età")
+
+#     # controllo se l'età super quella prevista dalla classe
+#     if (classe == 1) and anni > 11:
+#         bocciatura = True
+#     elif (classe == 2) and anni > 12:
+#         bocciatura = True
+#     elif (classe == 3) and anni > 13:
+#         bocciatura = True
+#     else:
+#         bocciatura = False
+    
+#     scheda_alunno = f"""
+#         ----- {alunno.get("nome")} {alunno.get("cognome")} ----
+#         età: {anni}
+#         classe/sezione: {classe}{alunno.get("sezione")}
+#         bocciata/o: {bocciatura}
+
+#     """
+#     print(scheda_alunno)
 
 
 
